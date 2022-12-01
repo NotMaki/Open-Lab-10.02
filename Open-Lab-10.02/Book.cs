1cf0515d130dd4613e1875507704eb9f6cf72a98
maki <jantulamarko@outlook.com>
2022-12-01 08:53:20 +0100

    Added more variables

--- Open-Lab-10.02/Book.cs
+++ Open-Lab-10.02/Book.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,12 +10,55 @@ namespace Open_Lab_10._01
 {
     internal class Book
     {
-        public string title;
+        private string title;
         private int pages;
-        public Book(string title, int pages)
+        private string category;
+        private string author;
+        private int releaseDate;
+        public string Title(string strtitle)
         {
-            this.title = title;
-            this.pages = pages;
+            this.title = strtitle;
+            return strtitle;
         }
+
+        public int Pages(int intpages)
+        {
+            this.pages = intpages;
+            return intpages;
+        }
+
+        public string Category(string strcategory)
+        {
+            this.category = strcategory;
+            return strcategory;
+        }
+
+        public string Author(string strauthor)
+        {
+            this.author = strauthor;
+            return strauthor;
+        }
+
+        public int ReleaseDate(int intreleaseDate)
+        {
+            this.releaseDate = intreleaseDate;
+            return intreleaseDate;
+        }
+
+        public void ans()
+        {
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Title :        " + Title(title));
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Pages :        " + Pages(pages));
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Category :     " + Category(category));
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Author :       " + Author(author));
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Release Date : " + ReleaseDate(releaseDate));
+            Console.WriteLine("----------------------------------------");
+        }
+
     }
 }
