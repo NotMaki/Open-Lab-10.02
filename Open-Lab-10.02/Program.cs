2b20c0b0011382637a4165c876831531c241d14e
maki <jantulamarko@outlook.com>
2022-12-01 07:59:25 +0100

    Add project files.

---
+++ Open-Lab-10.02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Open_Lab_10._01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book LOTR = new Book("the Lord of the Rings", 9250);
+            Console.WriteLine(LOTR.title);
+            //  LOTR.pages is private
+        }
+    }
+}
